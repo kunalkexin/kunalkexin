0c6a099ccd32fd79b38435c65b5cfe3bf3b0e02a
kunalkexin <88594828+kunalkexin@users.noreply.github.com>
2023-07-03 23:20:29 +0530

    Update settings.py

--- cgl/settings.py
+++ cgl/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-re=ik2un7nm&16qnfpj6=ouhax=33-bxf@t$kd9oq%i76gzvh#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
